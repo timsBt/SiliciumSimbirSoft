e0aeaf53a43a10c262d7ca50fb80825d0fe60fd8
timsBt <tims.bekmat@gmail.com>
2023-04-27 02:38:41 +0400

    ESTTingggg

--- src/test/java/TestCase2.java
+++ src/test/java/TestCase2.java
@@ -10,27 +10,14 @@ import java.time.Duration;
 public class TestCase2 {
 
     private WebDriver driver;
-
     private LoginPage loginPage;
-
-
 
     @BeforeEach
     public void SetUuuup() {
-
-    @BeforeEach
-    public void SetUp() {
-
-    @BeforeEach
-    public void SeyUp() {
-
-
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-
-
 
         driver.get("https://www.saucedemo.com/");
         loginPage = new LoginPage(driver);
@@ -40,7 +27,7 @@ public class TestCase2 {
 
 
     @Test
-    public void errorMessage(){
+    public void errorMessagessss(){
         String error = loginPage.errorText();
         Assertions.assertEquals("Epic sadface: Username and password do not match any user in this service" , error);
     }
@@ -49,6 +36,13 @@ public class TestCase2 {
     public void tearDown(){
         driver.quit();
     }
+
+
+
+
+
+
+
 }
 
 
